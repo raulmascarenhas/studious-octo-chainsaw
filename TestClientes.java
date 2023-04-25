e2a8e1de32b778c014f0f7b20e9c409b96201ad5
Raul Mascarenhas <54124047+raulmascarenhas@users.noreply.github.com>
2023-04-25 15:51:09 -0300

    Exercicio POO

---
+++ TestClientes.java
@@ -0,0 +1,17 @@
+package Poo;
+
+public class TestClientes {
+
+	public static void main(String[] args) {
+		
+		Clientes1 novoCliente =new Clientes1( "Jucelino",45, 1239666967, "Rua Generation 2023", "23235856565");
+		Clientes1 novoCliente2 =new Clientes1( "Maria ",55, 1225566967, "Rua Generation 2023", "122222233");
+	
+		novoCliente.visualizar();
+		novoCliente2.visualizar();
+		
+		
+		
+	}
+
+}
